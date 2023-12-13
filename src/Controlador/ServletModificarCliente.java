19716a3380e89a01bd14d77b687405c58b9c8b23
Tiago <tiagosantamaria01@gmail.com>
2023-12-13 20:32:57 -0300

    Edit Rol Cliente

---
+++ src/Controlador/ServletModificarCliente.java
@@ -0,0 +1,36 @@
+package Controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Data.ClienteData;
+import Entidades.Cliente;
+
+@WebServlet("/ServletModificarCliente")
+public class ServletModificarCliente extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ServletModificarCliente() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String usuarioCliente = request.getParameter("usuarioCliente");
+		Cliente cli = ClienteData.searchByUsername(usuarioCliente);
+		int rolDeseado = (cli.getEsAdmin() == 0) ? 1 : 0;
+		
+		ClienteData.updateRolCliente(cli.getDniCliente(), rolDeseado);
+		
+		request.getRequestDispatcher("/mainAdmin.jsp").forward(request, response);
+	}
+
+}
