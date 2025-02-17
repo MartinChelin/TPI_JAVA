4d766044a5db35d709473b9b5f579c10191605ef
MartinChelin <chelinmartin20@gmail.com>
2025-02-17 19:11:39 -0300

    CarritoNuevo
    Se crearon las clases del carrito nuevo y ItemCarrito

---
+++ src/Entidades/ItemCarrito.java
@@ -0,0 +1,34 @@
+package Entidades;
+
+public class ItemCarrito {
+	private Producto producto;
+	private int cantidad;
+	
+	public ItemCarrito(Producto producto, int cantidad) {
+		// TODO Auto-generated constructor stub
+		this.producto = producto;
+		this.cantidad = cantidad;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
